be341654fb379adb881b9d33622a48e190350bc9
SharonYkt <sharon825@gmail.com>
2022-04-08 13:38:35 +0530

    difference added

---
+++ Diff.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Diff
+{
+	public static void main(String args[])
+{
+	int b=5,c=10;
+	int diff=c-b;
+	System.out.println(diff);
+
+}
+}
---
+++ Prod.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Prod
+{
+	public static void main(String args[])
+{
+	int a=2,b=5,c=10;
+	int prod=a*b*c;
+	System.out.println(prod);
+
+}
+}
